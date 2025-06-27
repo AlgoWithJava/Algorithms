8be36e5fcad5ac2c5a56edab504a6c3ba38320b3
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-27 17:19:50 +0100

    Reverse names in GoodBbye

--- Module_1/HelloGoodbye.java
+++ Module_1/HelloGoodbye.java
@@ -3,7 +3,7 @@ public class HelloGoodbye {
         int length = args.length;
         if (length == 2){
             System.out.println(String.format("Hello %s and %s.", args[0], args[1]));
-            System.out.println(String.format("Goodbye %s and %s.", args[0], args[1]));
+            System.out.println(String.format("Goodbye %s and %s.", args[1], args[0]));
         }
     }
 }
