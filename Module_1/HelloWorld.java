7c1c1b3ef8236a8664e48213b257f3d1e63caf49
yamilobt8 <yamilo12345678910@gmail.com>
2025-06-27 17:12:56 +0100

    First Submuission

--- Module_1/HelloWorld.java
+++ Module_1/HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(String.format("the lenght of the arguments is: %d", args.length));
-
-        for(int i = 0; i < args.length; i++){
-            System.out.println(String.format("Hello, %s", args[i]));
-        }
+        System.out.println("Hello, World");
     }
 }
